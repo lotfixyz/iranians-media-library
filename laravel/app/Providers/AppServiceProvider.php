1d1ecc50ec7bd61cce5f0532b43e09ecf6694dd9
Mohammad Lotfi <info@lotfi.xyz>
2022-01-17 00:28:45 +0330

    Initialized.
    Based on:
    https://github.com/lotfixyz/empty_webapp_struct.git
    https://github.com/lotfixyz/laravel-1dir_5.8.git

---
+++ laravel/app/Providers/AppServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('path.public', function () {
+            return base_path() . '/../public_html';
+        });
+        if ('production' !== $this->app->environment())
+        {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Schema::defaultStringLength(191);
+    }
+}
